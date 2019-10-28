d44d0a9a33e0de9623bc3fb574bdd3a6c0747856
ait <ait@ex.com>
2019-10-28 22:13:44 +0900

    Private get set

--- app3.js
+++ app3.js
@@ -1,20 +1,26 @@
 // クラス
 // public, protected, private
 var User = /** @class */ (function () {
-    /*
-    public name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
-    */
-    function User(name) {
-        this.name = name;
+    function User(_name) {
+        this._name = _name;
     }
     User.prototype.sayHi = function () {
-        console.log("hi! i am " + this.name);
+        console.log("hi! i am " + this._name);
     };
+    Object.defineProperty(User.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (newValue) {
+            this._name = newValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return User;
 }());
 var tom = new User("Tom");
+console.log(tom.name);
+tom.name = "TOM";
 console.log(tom.name);
 tom.sayHi();
--- app3.ts
+++ app3.ts
@@ -2,19 +2,27 @@
 // public, protected, private
 
 class User {
-    /*
-    public name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
-    */
-    constructor(public name: string) {
+    constructor(private _name: string) {
     }
     public sayHi(): void {
-        console.log("hi! i am " + this.name);
+        console.log("hi! i am " + this._name);
+    }
+    get name(){
+       return this._name;
+    }
+    set name(newValue: string){
+        this._name = newValue;
     }
 }
 
 var tom = new User("Tom");
 console.log(tom.name);
+
+tom.name ="TOM";
+console.log(tom.name);
+
 tom.sayHi();
+
+
+
+

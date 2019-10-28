01ea5be4750b1381bb7a8289dee408e809f32941
ait <ait@ex.com>
2019-10-28 21:54:36 +0900

    st

---
+++ app.js
@@ -0,0 +1,5 @@
+function add(_a) {
+    var a = _a.a, b = _a.b;
+    return a + b;
+};
+console.log(add({ a: 1, b: 12 }));
---
+++ app.ts
@@ -0,0 +1,4 @@
+function add({ a, b }: { a: number; b: number; }): number {
+    return a + b;
+};
+console.log(add({ a: 1, b: 2 }));

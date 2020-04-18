846795a2a35d469ea56447387705d763841fff94
ait <ait@ex.in>
2020-04-18 17:34:21 +0900

    add array

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tabWidth: 2, //インデントのスペース数
+  singleQuote: true, //シングルクォートに統一
+  trailingComma: 'es5', //末尾のカンマ: es5に準拠させる。
+  semi: false
+};
---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {}
+
+let numbs: number[] = [1, 2, 3]
+
+let numbers2: Array<number> = [1, 2, 3]
+
+const stringArr: string[] = ['at', 'st', 'mod']
+
+const twoDimensional: number[][] = [
+  [2, 3],
+  [2, 7],
+]
+
+const obj: (string | number | boolean)[] = [1, false, 'mod']

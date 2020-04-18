e49503e4e0e9dde132d1d816fdb254354083bd0d
ait <ait@ex.in>
2020-04-18 19:23:37 +0900

    [setting] Prettier [ts] tuple Type Object

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   tabWidth: 2, //インデントのスペース数
   singleQuote: true, //シングルクォートに統一
-  trailingComma: 'es5', //末尾のカンマ: es5に準拠させる。
+  trailingComma: 'all', //末尾のカンマ: es5に準拠させる。
   semi: false
 };
---
+++ src/tuple.ts
@@ -0,0 +1,5 @@
+export {}
+
+let profile: [string, number] = ['Yamada', 32]
+// profile = [ 2, 'Yamada' ]; <- 型エラー
+profile = ['Yamada', 2]
